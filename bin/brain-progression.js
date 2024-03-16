82ea4bd3b7dfc20719ce75a1e8856b46aef63c6c
ultra59 <elenataft2016@gmail.com>
2024-03-16 18:01:58 +0300

    update tasks

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,27 +4,6 @@ import readlineSync from 'readline-sync';
 import { name } from '../src/cli.js';
 import { howdy, welldone } from '../src/utils.js';
 
-function generateArithmeticProgression(hiddenIndex) {
-  const progression = [];
-  const start = Math.floor(Math.random() * 10);
-  const diff = Math.floor(Math.random() * 5 + 3);
-  const length = Math.floor(Math.random() * 5 + 5);
-
-  let hiddenValue;
-  
-  for (let i = 0; i < length; i++) {
-    const newValue = start + i * diff;
-    
-    if (i === hiddenIndex) {
-      hiddenValue = newValue;
-      progression.push('..');
-    } else {
-      progression.push(newValue);
-    }
-  }
-
-  return { progression, hiddenValue };
-}
 export default function getProgression() {
   howdy();
   let count = 0;
