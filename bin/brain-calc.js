9039a5e0d9188bdac650c8b47ffff61c2be05d1c
ultra59 <elenataft2016@gmail.com>
2024-03-16 18:06:58 +0300

    update tasks

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -12,19 +12,19 @@ export default function calculateFunction() {
   let count = 0;
 
   const operator = ['+', '-', '*'];
-  
+
   const askQuestion = () => {
     const newValue = Math.round(Math.random() * 100);
     const newValue2 = Math.round(Math.random() * 20);
     const randomIndex = Math.floor(Math.random() * 3);
 
     const check = `${newValue} ${operator[randomIndex]} ${newValue2}`;
-     console.log(`Question: ${check}`);
-  
+    console.log(`Question: ${check}`);
+
     const answer = readlineSync.question('Your answer: ');
-  
-    if ((operator[randomIndex] === '+' && Number(answer) === newValue + newValue2) 
-        || (operator[randomIndex] === '-' && Number(answer) === newValue - newValue2)  
+
+    if ((operator[randomIndex] === '+' && Number(answer) === newValue + newValue2)
+        || (operator[randomIndex] === '-' && Number(answer) === newValue - newValue2)
         || (operator[randomIndex] === '*' && Number(answer) === newValue * newValue2)) {
       console.log('Correct!');
       count += 1;
@@ -32,16 +32,16 @@ export default function calculateFunction() {
       console.log(`'${answer}' is the wrong answer ;(. Correct answer was '${eval(check)}'. Let's try again, ${name}!`);
       return false;
     }
-    
+
     return true;
   };
-  
+
   for (let i = 0; i < 3; i += 1) {
     if (!askQuestion()) {
-      break; 
+      break;
     }
   }
-  
+
   if (count === 3) {
     welldone();
   }
