a6746f0bd135ed2c9eb4cc133ef98b92fd702b3e
ultra59 <elenataft2016@gmail.com>
2024-03-16 17:54:54 +0300

    update tasks

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint no-eval: 0 */
 /* eslint-disable no-unused-vars */
 import readlineSync from 'readline-sync';
 import { name } from '../src/cli.js';
@@ -9,45 +10,41 @@ export default function calculateFunction() {
   howdy();
   console.log('What is the result of the expression?');
   let count = 0;
-  for (let i = 0; i < 3; i += 1) {
+
+  const operator = ['+', '-', '*'];
+  
+  const askQuestion = () => {
     const newValue = Math.round(Math.random() * 100);
     const newValue2 = Math.round(Math.random() * 20);
     const randomIndex = Math.floor(Math.random() * 3);
-    const operator = ['+', '-', '*'];
 
     const check = `${newValue} ${operator[randomIndex]} ${newValue2}`;
-
-    console.log(`Question: ${check}`);
+     console.log(`Question: ${check}`);
+  
     const answer = readlineSync.question('Your answer: ');
-
-    if (operator[randomIndex] === '+' && Number(answer) === newValue + newValue2) {
+  
+    if ((operator[randomIndex] === '+' && Number(answer) === newValue + newValue2) 
+        || (operator[randomIndex] === '-' && Number(answer) === newValue - newValue2)  
+        || (operator[randomIndex] === '*' && Number(answer) === newValue * newValue2)) {
       console.log('Correct!');
       count += 1;
+    } else {
+      console.log(`'${answer}' is the wrong answer ;(. Correct answer was '${eval(check)}'. Let's try again, ${name}!`);
+      return false;
     }
-
-    if (operator[randomIndex] === '-' && Number(answer) === newValue - newValue2) {
-      console.log('Correct!');
-      count += 1;
-    }
-
-    if (operator[randomIndex] === '*' && Number(answer) === newValue * newValue2) {
-      console.log('Correct!');
-      count += 1;
-    } else if (operator[randomIndex] === '+' && Number(answer) !== newValue + newValue2) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${Number(newValue + newValue2)}'.Let's try again, ${name}!`);
-      break;
-    } else if (operator[randomIndex] === '-' && Number(answer) !== newValue - newValue2) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${Number(newValue - newValue2)}'.
-        Let's try again, ${name}!`);
-      break;
-    } else if (operator[randomIndex] === '*' && Number(answer) !== newValue * newValue2) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${Number(newValue * newValue2)}'.
-          Let's try again, ${name}!`);
-      break;
+    
+    return true;
+  };
+  
+  for (let i = 0; i < 3; i += 1) {
+    if (!askQuestion()) {
+      break; 
     }
   }
+  
   if (count === 3) {
     welldone();
   }
 }
+
 calculateFunction();
