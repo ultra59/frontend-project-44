5e5960803b0eb7c810719be856a3e256fcb05e62
ultra59 <elenataft2016@gmail.com>
2024-05-13 22:26:03 +0300

    update

--- bin/brain-games.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-// eslint-disable-next-line import/no-unresolved
-import * as getName from '../src/cli.js';
-// eslint-disable-next-line import/no-unresolved
-console.log('Welcome to the Brain Games!');
-getName.getUserName();
--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-// eslint-disable-next-line import/no-mutable-exports
-export let name = '';
-
-export const getUserName = () => {
-  name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
-};
