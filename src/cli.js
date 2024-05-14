c315d7c4a29423c6b85854bd1a8bf9aeef50c54d
root <root@DESKTOP-61SRCVD>
2024-05-14 16:38:43 +0300

    update

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
+
 export default function cli() {
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-return name;
-};
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+}
